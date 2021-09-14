3f76d0da4aaeeaffb38ebbc8746b187c08cb1f3e
Marco Preciado <mac46005@gmail.com>
2021-09-13 22:24:05 -0500

    M2 cssdisplay.html display.js demo

---
+++ Modules/js/display.js
@@ -0,0 +1,10 @@
+function removeElement(){
+    document.getElementById("div1").style.display = "none";
+}
+function changeVisibility(){
+    document.getElementById("div2").style.visibility = "hidden";
+}
+function resetElement(){
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.visibility = "visible";
+}
