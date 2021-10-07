0d430be8cb16e64e7934a68729f242d58d26807f
Marco Preciado <mac46005@gmail.com>
2021-10-06 23:10:06 -0500

    M4 Events

---
+++ Modules/js/events.js
@@ -0,0 +1,40 @@
+/*
+blur    onblur
+change  onchange
+click   onclick
+dbleclick   ondblclick
+focus   onfocus
+keydown onkeydown
+keypress    onkeypress
+keyup   onkeyup
+load onload
+mousedown onmousedown
+mousemove   onmousemove
+mouseout    onmouseout
+mouseover   onmouseover
+reset   onreset
+select  onselect
+submit  onsubmit
+unload  onunload
+
+Event attributes and their tags
+onblur  a,button,input,textarea,select,
+onchange    input,textarea,select
+onclick a,input
+ondblclick  most elements
+onfocus a,input,textarea,select
+onkeydown   body,form elements
+onkeypress  body,form elements
+onkeyup body,form elements
+onload  body
+onmousedown most elements
+onmousemove most elements
+onmouseout most elements
+onmouseover most elements
+onmouseup most elements
+onreset form
+onselect    input
+onsubmit    form
+onunload body
+ */
+
