4c9f086b51f393cc419df286093462ab60c8089b
Marco Preciado <mac46005@gmail.com>
2021-10-28 21:57:27 -0500

    PHP functions

---
+++ Modules/M5_php/Functions.php
@@ -0,0 +1,35 @@
+<!--
+    The real pwer of PHP comes from its functions. PHP has more than 1000 builtin functions
+    See: http://php.net/manual/en/indexes.functions.php for a full listing
+    *************************************
+    PHP User Defined Functions
+
+    Besides the built in PHP funtions, we can create our own functions.
+
+    A function is a block statements that can be used repeatedly in a program
+
+    A function will no execute immediately when a page loads
+
+    A function will be executed by a call to the function
+
+    A function name can start with a letter or underscore (not a number)
+
+    Function names are NOT case-sensitive
+
+    Tip: Give the function a name that reflects what the function does!
+    ****************************
+    PHP Function arguments
+    Information can be passed to functions through arguments. An argument
+    is just like a variable
+
+    Arguments are specified after the funtion name, inside parantheses
+    You can add as many arguments as you want, just separate them with a comma
+
+    **************************
+    PHP default argument value
+
+    arguments can be specified with a default parameter
+    *************************
+    PHP functions - returning values
+    To let a function return a value , use the return statement
+-->
---
+++ Modules/M5_php/Operators.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php define("title","Operators")?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo title;?></title>
+</head>
+<body>
+    <!--
+        PHP Operators
+        OPerators are used to perform operations on vairables and values.
+        PHP divides the operators in the following groups:
+
+        Arithmetic operators
+        Assignment operators
+        Comparison operators
+        Increment/decrement operators
+        Logical operators
+        String operators
+        Array operators
+
+        ********************
+        PHP Arithmetic Operators
+
+        The PHP arithmetic operators are used with numeric values
+        to perform common arithmetical operations, such as addition,
+        subtraction, multiplication etc.
+
+        Operator : Name
+        + : Addition
+        - : Subtraction
+        * : Multiplication
+        / : Division
+        % : Modulus
+        ** : Exponentation
+        **********************
+        PHP assignment Operators
+
+        The PHP assignment operators are used with numeric values to write a value to a variable
+
+        The basic assigment operator in PHP is "=". It means that the left operand gets set to the value of the assignment expression
+        on the right
+
+        Assigment : Same as
+        x=y : y=x
+        x+=y : x=x+y
+        x-=y : x=x-y
+        x*=y : x=x*y
+        x/=y : x=x/y
+        x%=y : x=x%y
+        x**=y : x=x**y
+        ****************************
+        PHP Comparison Operators
+
+        The PHP comparison operators are used to compare two values
+        (number or string)
+
+        Operator : Name 
+        == : Equal
+        === : Identical
+        != : Not equal
+        <> : Not equal
+        !=== : Not identical
+        > : Greater than
+        < : Less than
+        >= : Greater than or equal to
+        <= : Less than or equal to
+
+        ***************************
+        PHP Increment / Decrement Operators
+
+        The PHP increment operators are used to incrememnt a variable's value
+        The PHP decrement operators are used to decrement a variable's value
+
+        Operator : Name : Description
+        ++$x : Pre-increment : Incremenets x by one, then returns x
+        $x++ : Post-increment : returns x, then increments by one
+        --$x : Pre-decrement : Increments x by one, then returns x
+        $x-- : Post-decrement : Returns x, then increments by one
+
+        ****************************
+
+        PHP Logical Operators
+
+        The PHP Logical operators are used to combine conditional statements
+
+        Operator : Name
+        and : AND
+        or : OR
+        xor : XOR
+        && : AND
+        || : OR
+        ! : NOT
+
+        *************************
+        PHP String Operators
+        PHP has two operators that are specially designed for strings
+
+        Operator : Name
+        . : Concatenation
+        .= : Concatenation assignment
+        *****************************
+        PHP Array Operators
+
+        The PHP array operators are used to compare arrays
+
+        Operator : Name
+        + : Union
+        == : Equality
+        === : Identical
+        != : Inequality
+        <> : Inequality
+        !== : Non-Identical
+        *****************************
+        
+    -->
+</body>
+</html>
