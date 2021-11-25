f8b45e37260496d2047e0896adf1131543d90543
Marco Preciado <mac46005@gmail.com>
2021-11-24 20:20:37 -0600

    Changing the contents of the xml file

--- Modules/M6_xml/index.php
+++ Modules/M6_xml/index.php
@@ -27,6 +27,37 @@
     echo $userModel->gender . "<br/>";
     echo $userModel->maritalStatus . "<br/>";
     echo $userModel->dob . "<br/>";
+
+    $userModel->id = 2000;
+    $userModel->userName = "Jose5545";
+    $userModel->password = "thebestPasswordEver";
+    $userModel->firstName = "Jose";
+    $userModel->lastName = "Preciado";
+    $userModel->address01 = "Unknown";
+    $userModel->address02 = "307 waters st.";
+    $userModel->city = "Saint Louis";
+    $userModel->state = "MO";
+    $userModel->zip = "632541";
+    $userModel->phoneNumber = "3456646464";
+    $userModel->email = "someemail@email.com";
+    $userModel->gender = "Female";
+    $userModel->maritalStatus = "Single";
+    echo "<br/><br/><br/>";
+    echo $userModel->id . "<br/>";
+    echo $userModel->userName . "<br/>";
+    echo $userModel->password . "<br/>";
+    echo $userModel->firstName . "<br/>";
+    echo $userModel->lastName . "<br/>";
+    echo $userModel->address01 . "<br/>";
+    echo $userModel->address02 . "<br/>";
+    echo $userModel->city . "<br/>";
+    echo $userModel->state . "<br/>";
+    echo $userModel->zip . "<br/>";
+    echo $userModel->phoneNumber . "<br/>";
+    echo $userModel->email . "<br/>";
+    echo $userModel->gender . "<br/>";
+    echo $userModel->maritalStatus . "<br/>";
+    echo $userModel->dob . "<br/>";
     ?>
 </body>
 </html>
