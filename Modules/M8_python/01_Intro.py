db4c886c6c26d6f6a42c09791f85fe6e18454145
Marco Preciado <mac46005@gmail.com>
2021-12-01 14:19:59 -0600

    Intor to python

---
+++ Modules/M8_python/01_Intro.py
@@ -0,0 +1,35 @@
+# To setup using Python with IntelliJ, first download the Python plugin
+# Go to File -> Settings -> Plugins and search for the Python By JetBrains
+# Then install and restart IntelliJ to activate the plugin
+#https://www.jetbrains.com/help/idea/plugin-overview.html
+
+#Go to file -> Project Structure -> New Python SDK -> System Interpreter
+# and put the full path to the python interpreter on your machine.
+
+# if you need to download python go here https://www.python.org/downloads/
+
+# what is Python?
+
+# Python is a popular programming language. It was created by Guido can Rossum,
+# and released in 1991
+
+# It is used for:
+# Web development (server-side),
+# software development,
+# mathematics,
+# system scripting
+
+# python was designed for readability
+# and has some similarities to the English language with
+# influence from mathematics
+
+# Python uses new lines to complete a command,
+# as opposed to other programming languages which often
+# use semicolons or parentheses.
+
+# Python relies on indentation, using whitespace,
+# to define scope; such as the scope of loops,
+# functions and classes. Other porgramming languages
+# Often use curly brackets for this purpose
+
+print("Hello World!")
