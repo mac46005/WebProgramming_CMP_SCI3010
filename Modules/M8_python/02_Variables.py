f23582965678ae3ae3472183bff0726d297b1670
Marco Preciado <mac46005@gmail.com>
2021-12-01 14:29:50 -0600

    Python Variables

---
+++ Modules/M8_python/02_Variables.py
@@ -0,0 +1,32 @@
+"""
+Unlike other programming languages,
+Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it.
+
+Variables do not need to be declared with any particular type
+and can even change type after they have been set.
+"""
+
+x = 5;
+print(x)
+x = "Marco"
+print(x)
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha numeric characters
+and underscores (A-z, 0-9, and _)
+Variable names are case sensitive ( age,Age,and AGE are three different variables)
+
+"""
+
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
+x = 5
+y = "John"
+print(x + y)
+
