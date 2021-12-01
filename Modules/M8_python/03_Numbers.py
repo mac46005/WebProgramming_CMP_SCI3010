6ec3714d896d00f6b86f20f99b0243af21e8dd0c
Marco Preciado <mac46005@gmail.com>
2021-12-01 14:43:40 -0600

    Numbers Python

---
+++ Modules/M8_python/03_Numbers.py
@@ -0,0 +1,35 @@
+"""
+There are three numeric types in python
+
+int
+float
+complex
+
+Variables of numeric types are created when you assign a value to them
+
+To verify the type of any object in Python, use the type() function
+"""
+
+# Int, or integer, is a whole number, positive or negative,
+# without decimals, of unlimited length
+
+x = 89839048598093849058;
+
+# Float , or floating point number is a number, positive or negative,
+# containing one or more decimals. Float can also be scientific numbers
+# with an "e" to indicate the power of 10
+
+y = -87.7e100
+
+# Complex numbers are written with a "j" as the imaginary part
+
+z = 3+5j
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
