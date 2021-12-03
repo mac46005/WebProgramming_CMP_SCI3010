4424ca17a4869a6086ba8c5327abae579760f7f7
Marco Preciado <mac46005@gmail.com>
2021-12-02 18:24:01 -0600

    Operators Python

---
+++ Modules/M8_python/06_Operators.py
@@ -0,0 +1,27 @@
+"""
+Operators are used to perform operations on variables and values
+
+Python divides the operators in the following groups:
+
+Arithmetic
+Assignment
+Comparison
+Identity
+Membership
+"""
+
+x = 4
+y = 3
+
+# Arithmetic Operators
+
+print("Arithmetic Operators")
+print(str(x) + "+" + str(y) + " = " + str(x + y))
+print(str(x) + "-" + str(y) + " = " + str(x - y))
+print(str(x) + "*" + str(y) + " = " + str(x * y))
+print(str(x) + "/" + str(y) + " = " + str(x / y))
+print(str(x) + "%" + str(y) + " = " + str(x % y))
+print(str(x) + "**" + str(y) + " = " + str(x ** y))
+print(str(x) + "//" + str(y) + " = " + str(x // y))
+
+
